5e16f593810430bb60faf02fcc43a9db4502d117
pralhad677 <pralhadkharel@gmail.com>
2021-04-04 11:18:37 -0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Style from '../styles/Home.module.css'
+
+import  {NextPage} from 'next'
+
+interface InitialProps {
+  
+}
+interface Props extends InitialProps {}
+
+let Index:NextPage<Props,InitialProps>=(props)=> {
+  return (
+    <div>
+      <h1 className={Style.h1}>hi jacob</h1>
+    </div>
+  )
+}
+Index.getInitialProps = async () => ({
+ 
+})
+
+export default Index
