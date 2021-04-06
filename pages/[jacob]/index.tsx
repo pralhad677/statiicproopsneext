23910ab9728acd7f4969605180449ab9983dfe4c
pralhad <51488927+pralhad677@users.noreply.github.com>
2021-04-06 00:09:36 -0700

    diff betwn context and useRouter

--- pages/[jacob]/index.tsx
+++ pages/[jacob]/index.tsx
@@ -18,6 +18,11 @@ let Hello:NextPage<Props,InitialProps>=(props)=> {
 }
 
 export async function getStaticProps(context): Promise<GetStaticPropsResult<InitialProps>> {
+  //haile main herunu parne kura cahi k ho vanda kheri ni dyamic page ma hamile dyanamic aprt id lai acces agrne techniuq vanko chai 2 ta x vanum
+  //euta chai useRouter ko through bata router and then router.query.dynamicId(pid)
+  
+  //arko vaneko chai getStaticProps ma 
+  //context le params dido rexa ani tesbata direct params.pid grda hunxa
     const {params} = context
     console.log(params)
 return  
