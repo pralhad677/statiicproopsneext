9931d2b1fc3f8cba6f7a87f4e07e55c798a04fe8
pralhad677 <pralhadkharel@gmail.com>
2021-04-10 11:57:04 -0700

    after resolving execution order and passing exact data

--- pages/[pug].tsx
+++ pages/[pug].tsx
@@ -6,14 +6,14 @@ import path from 'path'
 import jsonfile from 'jsonfile'
 
 import  {NextPage,GetStaticPropsResult,GetStaticPathsResult} from 'next'
-import { array } from '../data/dummy.backend';
+// import { array } from '../data/dummy.backend';
 
 interface Data {
   name:string,
   id:string
 }
 interface InitialProps{
-    array:Data[]
+    array:Array<Data>
 }
 // let d:InitialProps = {
 //     array: [
@@ -35,6 +35,9 @@ let Pug:NextPage<Props,InitialProps>=(props)=> {
   return (
     <div>
       <h1 className={Style.h1}>hi jacob</h1>
+      {props.array.map(item=>{
+        return <li key={item.id}>{item.name}:{item.id}</li>
+      })}
     </div>
   )
 }
@@ -45,25 +48,16 @@ let Pug:NextPage<Props,InitialProps>=(props)=> {
 export async function getStaticProps(context): Promise<GetStaticPropsResult<InitialProps>> {
   let {params} = context
   let dynamicId = params.pug
-  var arr:InitialProps;
-  let myFUnc =(data)=>{
-    arr=data
-    console.log('arr',arr.array)
-  }
-  console.log('arr',arr)
+ 
   
     try{
-    const Path = path.resolve(process.cwd(),'data','array.txt')//you can use path.join() too
+    var Path = path.resolve(process.cwd(),'data','array.txt')//you can use path.join() too
     //process.cwd => gives current working directory and then using 'data' to move into data folder inside of working directory
     console.log(Path)
     var data1:string= (await fs.readFile(Path)).toString()
     //    var data2 = data1.array
     console.log('data1',data1)
-    jsonfile.readFile(Path, function (err, obj) {
-      if (err) console.error(err)
-      // console.log( obj.array.forEach(x=>console.log(x)))
-      myFUnc(obj)
-    })
+   
 
     }catch(err){
       console.log(err)
@@ -81,14 +75,38 @@ export async function getStaticProps(context): Promise<GetStaticPropsResult<Init
         notFound:true
       }
     }
-    // let array = arr.array.find(item=>item.id ===dynamicId)
-    console.log('arrayOnly',array)
+    console.log('arrayOnly')
     //execution order confusing x
+
+
+
+    //ya hamile json.readFile grda ni hunxa but tyo async task vako le getStaticProps le surumai return object grdina ani tespaxi matra json,redFile le data return grxa so readFileSync use grya ho
+    let myData = jsonfile.readFileSync(Path)
+    console.group('myData',myData)
+    
+    let array1 = myData.array.find(item=>item.id ===dynamicId)
+    console.log(array1)
+    // var arr:InitialProps;
+    // jsonfile.readFile(Path, function (err, obj) {
+    //   if (err) console.error(err)
+    //   // console.log( obj.array.forEach(x=>console.log(x)))
+    //   myFUnc(obj)
+    // })
+    // let myFUnc =(data)=>{
+    //   arr=data
+    //   console.log('arr',arr.array)
+    // }
+    // console.log('arr',arr)
+
+    
+   
     return {
+      
         props: {
-          // array
-           array:[{name:'jacob',id:'1'}],
-          // array:arr.array
+          array:[array1]
+        
+          //  array:[{name:'jacob',id:'1'}],
+        
             
         },
         revalidate:10,
